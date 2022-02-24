3e023aca3af64eb22a2df628df08305454b02c03
yashwant <yash.mangate1@gmail.com>
2022-02-24 20:05:21 +0530

    added return function --all working fine

--- Student_Library/script.js
+++ Student_Library/script.js
@@ -1,14 +1,19 @@
 console.log("bloody hell you are right !");
-ShowBook();
-function Book(name, book_name, auther, type) {
-  this.name = name;
-  this.book_name = book_name;
-  this.auther = auther;
-  this.type = type;
-}
-// adding books to websites data
 
+ShowBook();
+
+class Book {
+  constructor(name, book_name, auther, type) {
+    this.name = name;
+    this.book_name = book_name;
+    this.auther = auther;
+    this.type = type;
+  }
+}
+
+// adding books to websites data
 function ShowBook() {
+  console.log("running show book");
   let local_data = localStorage.getItem("Entry");
   let t_body = document.getElementById("tableBody");
   let uistring = "";
@@ -18,55 +23,43 @@ function ShowBook() {
     localObj = JSON.parse(local_data);
   }
   localObj.forEach((element, index) => {
+    // return_book(this.id) -> this will automatically pass the id of the current button to the function
     uistring += `<tr class="Student_data">
-                   <td >${index + 1}</td>
-                   <td >${element.name}</td>
+                   <td>${index + 1}</td>
+                   <td>${element.name}</td>
                    <td>${element.book_name}</td>
                    <td>${element.auther}</td>
                    <td>${element.type}</td>
                    <td>${element.issue_date}</td>
                    <td>
-                   <div class="col-sm-10">
-                    <button type="submit" id="retbtn" class="btn btn-danger">Return</button>
-                  </div>
-                       </td>
-                       </tr>`;
+                      <div class="col-sm-10">
+                        <button type="submit" id=${index} onClick=return_book(this.id) class="btn btn-danger">Return</button>
+                      </div>
+                   </td>
+                  </tr>`;
   });
-  t_body.innerHTML = uistring;
+  if (localObj.length == 0) {
+    // add class to this and enhance the look
+    t_body.innerHTML = "<h2>NO BOOKS TO SHOW</h2>";
+  } else {
+    t_body.innerHTML = uistring;
+  }
 }
 
 //fuction to create return book entry
-function return_book(index){
+function return_book(index) {
+  console.log("deleting an book of index", index);
+  let bookObj;
   let local_data = localStorage.getItem("Entry");
   if (local_data == null) {
-    localObj = [];
+    bookObj = [];
   } else {
-    localObj = JSON.parse(local_data);
+    bookObj = JSON.parse(local_data);
   }
-  localObj.splice(index,1);
-  localStorage.setItem("Entry", JSON.stringify(localObj));
+  bookObj.splice(index, 1);
+  localStorage.setItem("Entry", JSON.stringify(bookObj));
   ShowBook();
 }
-//function to search exact book entry
-
-function searchBook() {
-  console.log("your searching something!");
-  let search_text = document.getElementById("searchTxt");
-  let input = search_text.value.toLowerCase();
-  let Student_data = document.getElementById("tableBody");
-  console.log(Student_data);
-  // Array.from(Student_data).forEach(element => {
-  //   console.log(Student_data);
-  //   let name_txt = element.getElementsByTagName("td")[0].innerText;
-  //   if(name_txt.includes(input)){
-  //     element.style.display="block";
-  //   }
-  //   else{
-  //     element.style.display="none"
-  //   }
-  // });
-}
-// creating one function to clear and validate given data
 
 class Display {
   // checking validation given data is validate or not
@@ -151,18 +144,4 @@ addbtn.addEventListener("click", function (e) {
     display.msg("danger", `something wents wrong check your book entry`);
   }
   display.reset_form();
-});
-
-//creating a search book entry function
-let search = document.getElementById("searchTxt");
-search.addEventListener("click", function (e) {
-  e.preventDefault();
-  searchBook();
-});
-
-//creating a return book entry function
-let return_btn = document.getElementById("retbtn");
-return_btn.addEventListener("click", function (e){
-  e.preventDefault();
-  return_book()
 });
